ba377202ebcb6548f76a33c76cdb74cf5dc0605d
Raul Tomita <raul.tomita@RTOMITA>
2017-07-18 14:11:18 +0300

    do nothing

--- FarmMonitorServer/Startup.cs
+++ FarmMonitorServer/Startup.cs
@@ -12,19 +12,18 @@ using StackExchange.Redis;
 namespace FarmMonitorServer
 {
     public class Startup
-    {
+    {       
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(s=> ConnectionMultiplexer.Connect("192.168.1.200"));
-            services.AddLogging(l => l.AddConsole());
-            
+            services.AddSingleton(s=> ConnectionMultiplexer.Connect("192.168.1.200"));            
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseWebSockets();
             app.UseMvc();
         }
     }
