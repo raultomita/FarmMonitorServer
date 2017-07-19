5570e812f761bb8f9a0da0a7ca3bbc40814deb0d
Raul Tomita <raul.tomita@gmail.com>
2017-07-19 23:31:35 +0300

    Query devices from Redis and implement put for commands

--- FarmMonitorServer/Controllers/DevicesController.cs
+++ FarmMonitorServer/Controllers/DevicesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System;
 using System.Collections.Generic;
 
 namespace FarmMonitorServer.Controllers
@@ -6,9 +9,32 @@ namespace FarmMonitorServer.Controllers
     [Route("api/[controller]")]
     public class DevicesController : Controller
     {
+        private const string HashKey = "devices";
+        private readonly IDatabase database;
+        private readonly ISubscriber subscriber;
+
+        public DevicesController(ConnectionMultiplexer connection)
+        {
+            if(connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+
+            database = connection.GetDatabase();
+            subscriber = connection.GetSubscriber();
+        }
+
+        [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new[] { "1", "2" };
+            var devices = database.HashGetAll(HashKey);
+            return devices.Select(device => (string)device.Value);
+        }
+                
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody]string value)
+        {
+            subscriber.Publish("commands", id);
         }
     }
 }
