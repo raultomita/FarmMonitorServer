9788c5260fb2850399c75484334002444d2d09a5
Raul Tomita <raul.tomita@gmail.com>
2017-07-31 23:55:26 +0300

    Stop and remove current container

--- Deployment/deploy.ps1
+++ Deployment/deploy.ps1
@@ -1,3 +1,6 @@
 rm publish -r
 dotnet publish -r linux-arm -c Release -o Deployment\publish ..\FarmMonitorServer.csproj
-docker build -t farmmonitorweb:0.10 .
+docker container stop farmmonitorweb
+docker container rm farmmonitorweb
+docker build -t farmmonitorweb:latest .
+docker run --restart unless-stopped -p 80:5000  --name farmmonitorweb farmmonitorweb:latest
